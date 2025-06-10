3a7cc6dd6796cc74586f4d08a1ed0119f094234c
Justin Kramer <jpk91@pitt.edu>
2025-06-10 18:49:18 -0400

    Fix page

--- stock-analysis-app/frontend/vite.config.ts
+++ stock-analysis-app/frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Code-Side-Projects/',
+  base: '/Stock-Analysis-App/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
